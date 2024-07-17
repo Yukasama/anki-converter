17e4dde7eaa1ae06dc461285da7d115de77a87b6
Yukasama <yukasamaa@gmail.com>
2024-07-17 14:50:52 +0200

    fix: add headers

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,11 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
-            : "https://anki-converter.zenathra.com/api/",
+            : "https://anki-converter.zenathra.com/api/:path*",
+        headers:
+          process.env.NODE_ENV !== "development"
+            ? { "X-Forwarded-Host": "nextjs" }
+            : {},
       },
     ];
   },
