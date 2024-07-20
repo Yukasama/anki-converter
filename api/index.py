d7fcdd44a454a1eaa9325237972d56d49e2b787d
Yukasama <yukasamaa@gmail.com>
2024-07-20 14:20:56 +0200

    fix: writing file to bytes io

--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import logging
 import genanki
-import os
+import io
 from utils.markdown_to_anki import parse_markdown, create_anki_deck
 
 app = FastAPI()
@@ -30,17 +30,14 @@ async def create_anki_deck_from_markdown(file: UploadFile = File(...)):
         cards = parse_markdown(markdown_content)
         deck = create_anki_deck('Generated Deck', cards)
 
-        output_file = 'generated_deck.apkg'
+        output_buffer = io.BytesIO()
         anki_pkg = genanki.Package(deck)
-        anki_pkg.write_to_file(output_file)
-        
+        anki_pkg.write_to_file(output_buffer)
+        output_buffer.seek(0)
+
         logging.debug("Generated Anki package successfully")
 
-        if not os.path.exists(output_file):
-            logging.error(f"File {output_file} does not exist.")
-            return {"error": "Generated file not found."}
-
-        return FileResponse(output_file, media_type='application/octet-stream', filename=output_file)
+        return StreamingResponse(output_buffer, media_type='application/octet-stream', headers={'Content-Disposition': 'attachment; filename=generated_deck.apkg'})
     except Exception as e:
         logging.error(f"Error processing file: {str(e)}")
         return {"error": str(e)}
